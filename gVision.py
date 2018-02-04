89c460041cb5a8fee1797cb47cec608b9f71b3b3
Noah Abbott <abbottn3@gmail.com>
2018-02-04 13:55:04 -0500

    Searching for web_entities

--- gVision.py
+++ gVision.py
@@ -11,14 +11,18 @@ import os
 # Imports the Google Cloud client library
 from google.cloud import vision
 from google.cloud.vision import types
-
+'''
+googlecredstxt = open('googlecreds.txt','r')
+googlecreds = googlecredstxt.read()
+googlecredstxt.close()
+'''
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate
 file_name = os.path.join(
     os.path.dirname(__file__),
-    'Capture.PNG')
+    'Adam.JPG')
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
@@ -30,6 +34,15 @@ image = types.Image(content=content)
 response = client.label_detection(image=image)
 labels = response.label_annotations
 
+
 print('Labels:')
 for label in labels:
     print(label.description)
+
+webnotes = response.web_detection
+if webnotes.web_entities:
+	if webnotes.web_entities[0].score > 1:
+		print('Description: {}'.format(webnotes.web_entities[0].description))
+	else:
+		print('Score      : {}'.format(entity.score))
+        print('Description: {}'.format(entity.description))
